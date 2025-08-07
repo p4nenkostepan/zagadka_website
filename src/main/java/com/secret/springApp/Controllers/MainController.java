51edaccd18e33bc37174b81894ce5110270ba65b
mollen <stepanpanenko@gmail.com>
2025-08-07 14:15:48 +0300

    html basic main page, repaired MainController.java, SpringAppApplication.java

--- src/main/java/com/secret/springApp/Controllers/MainController.java
+++ src/main/java/com/secret/springApp/Controllers/MainController.java
@@ -1,14 +1,16 @@
 package com.secret.springApp.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController
 {
     @GetMapping("/")
     public String homePage(Model model)
     {
         model.addAttribute("title", "Main page");
-        return "homePage";
+        return "homePage"; // name
     }
 }
--- src/main/java/com/secret/springApp/SpringAppApplication.java
+++ src/main/java/com/secret/springApp/SpringAppApplication.java
@@ -2,9 +2,11 @@ package com.secret.springApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringAppApplication {
+public class SpringAppApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
